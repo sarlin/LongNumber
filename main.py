b4e96434ba095ecfb73c93a48a23f143a0ae9870
sarlin <sarlinp94@gmail.com>
2015-06-21 00:59:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import LN_Class
+
+import ClassLong
 import sys
 import os
 
-if ((len(sys.argv) < 5) || (len(sys.argv) > 7) :
+if len((sys.argv) < 5) || ((sys.argv) > 7)):
     print "Error: Wrong arguments' number"
     sys.exit(0)
 
@@ -11,7 +12,7 @@ if not os.path.exists(sys.argv[1]):
     sys.exit(0)
 
 if ((len(sys.argv[2]) > 1 or sys.argv[2][0] == '\0') or sys.argv[2][0] != '+' and sys.argv[2][0] != '-' and sys.argv[2][0] != '*' and sys.argv[2][0] != '/' and sys.argv[2][0] != '%' and sys.argv[2][0] != '^') :
-    print "Error: Unknown operation: ", sys.argv[2][0]
+    print "Error: Wrong operation: ", sys.argv[2][0]
     sys.exit(0)   
 
 if not os.path.isfile(sys.argv[3]) :
@@ -26,7 +27,7 @@ bin = 0
 
 if len(sys.argv) == 5 :
     if sys.argv[2][0] == '^' :
-        print "Error: module file is missed"
+        print "Error: Input module file"
         sys.exit(0)
 
 if len(sys.argv) == 6 :
@@ -52,9 +53,11 @@ if len(sys.argv) == 7 :
 
     bin = 1
 
-a = LN_Class.LN_Class()
+a = ClassLong.ClassLong()
 
-b = LN_Class.LN_Class()
+b = ClassLong.ClassLong()
+
+result = ClassLong.ClassLong()
 
 if bin == 1 :
     a.ReadBin(sys.argv[1])
@@ -65,9 +68,7 @@ if bin == 1 :
     b.ReadBin(sys.argv[3])
 else :
     b.ReadText(sys.argv[3])
-    
-result = LN_Class.LN_Class()
-    
+
 if sys.argv[2][0] == '+' :
     result = a + b
 
@@ -84,13 +85,13 @@ if sys.argv[2][0] == '%' :
     result = a % b
     
 if sys.argv[2][0] == '^' :
-    c = LN_Class.LN_Class()
+    c = ClassLong.ClassLong()
     if bin == 1 :
 	c.ReadBin(sys.argv[5])
     else :
 	c.ReadText(sys.argv[5])
 
-    result = LN_Class.PowMod(a, b, c)
+    result = ClassLong.PowMod(a, b, c)
   
 if bin == 1 :
     result.WriteBin(sys.argv[4])
