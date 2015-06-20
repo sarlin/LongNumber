dad89d297262db24af60e43480b5bfac25a6c1e2
sarlin <sarlinp94@gmail.com>
2015-06-21 00:57:12 +0700

    Update LibraryLong.h

--- LibraryLong.h
+++ LibraryLong.h
@@ -14,23 +14,20 @@ struct LongNumber ReadBinFile(const char* file);
 void WriteBinFile(const char* file, struct LongNumber number);
 struct LongNumber ReadTextFile(const char* file);
 void WriteTextFile(const char* file, struct LongNumber number);
-
+struct LongNumber Clear(struct LongNumber number);
+struct LongNumber Allocate(struct LongNumber number, unsigned int size);
+struct LongNumber Zero(struct LongNumber number, unsigned int size);
+struct LongNumber Copy(struct LongNumber from);
+struct LongNumber ToNorm(struct LongNumber a);
+int Compare(struct LongNumber a, struct LongNumber b);
+struct LongNumber Shift(struct LongNumber a, unsigned int s);
+struct LongNumber MulSmall(struct LongNumber a, unsigned long long int b);
+struct LongNumber DivSmall(struct LongNumber a, unsigned long long int b);
 struct LongNumber Sum(struct LongNumber a, struct LongNumber b);
 struct LongNumber Dif(struct LongNumber a, struct LongNumber b);
 struct LongNumber Mul(struct LongNumber a, struct LongNumber b);
 struct LongNumber Div(struct LongNumber a, struct LongNumber b, int sumbols);
 struct LongNumber Deg(struct LongNumber a, struct LongNumber b, struct LongNumber c);
-
-struct LongNumber DivSmall(struct LongNumber a, unsigned long long int b);
-struct LongNumber MulSmall(struct LongNumber a, unsigned long long int b);
-struct LongNumber Shift(struct LongNumber a, unsigned int s);
-struct LongNumber Clear(struct LongNumber number);
-struct LongNumber Allocate(struct LongNumber number, unsigned int size);
-struct LongNumber Zero(struct LongNumber number, unsigned int size);
-int Compare(struct LongNumber a, struct LongNumber b);
-struct LongNumber Copy(struct LongNumber from);
-struct LongNumber ToNorm(struct LongNumber a);
-
 struct LongNumber ReadNum(unsigned long long int value);
+char* PrintNum(struct LongNumber number); 
 struct LongNumber ReadStr(const char* num);
-char* PrintNum(struct LongNumber number);
