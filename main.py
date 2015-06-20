998856db18628ff3cc3500e7e2c663df40ff5e65
sarlin <sarlinp94@gmail.com>
2015-06-20 20:41:09 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,100 @@
+import ClassLong
+import sys
+import os
+
+if ((len(sys.argv) < 5) or (len(sys.argv) > 7) :
+    print "Error: Wrong arguments' number"
+    sys.exit(0)
+
+if not os.path.exists(sys.argv[1]):
+    print "Error: Unable to open file: ", sys.argv[1]
+    sys.exit(0)
+
+if ((len(sys.argv[2]) > 1 or sys.argv[2][0] == '\0') or sys.argv[2][0] != '+' and sys.argv[2][0] != '-' and sys.argv[2][0] != '*' and sys.argv[2][0] != '/' and sys.argv[2][0] != '%' and sys.argv[2][0] != '^') :
+    print "Error: Unknown operation: ", sys.argv[2][0]
+    sys.exit(0)   
+
+if not os.path.isfile(sys.argv[3]) :
+    print "Error: Unable to open file: ", sys.argv[3]
+    sys.exit(0)
+
+if not os.path.isfile(sys.argv[4]) :
+    print "Error: Unable to open file: ", sys.argv[4]
+    sys.exit(0)
+
+bin = 0
+
+if len(sys.argv) == 5 :
+    if sys.argv[2][0] == '^' :
+        print "Error: module file not found"
+        sys.exit(0)
+
+if len(sys.argv) == 6 :
+    if sys.argv[2][0] == '^' :
+	if not os.path.isfile(sys.argv[5]) :
+	    print "Error: Unable to open file: ", sys.argv[5]
+	    sys.exit(0)
+    else :
+        if sys.argv[5] != "-b" : 
+	    print "Error: Invalid flag: ", sys.argv[5]
+	    sys.exit(0)
+	    
+	bin = 1
+
+if len(sys.argv) == 7 : 
+    if not os.path.isfile(sys.argv[5]) :
+        print "Error: Unable to open file: ", sys.argv[5]
+	sys.exit(0)
+
+    if sys.argv[6] != "-b" :
+        print "Error: Invalid flag: ", sys.argv[6]
+        sys.exit(0)
+
+    bin = 1
+
+a = ClassLong.ClassLong()
+
+b = ClassLOng.ClassLong()
+
+if bin == 1 :
+    a.ReadBin(sys.argv[1])
+else :
+    a.ReadText(sys.argv[1])
+    
+if bin == 1 :
+    b.ReadBin(sys.argv[3])
+else :
+    b.ReadText(sys.argv[3])
+    
+result = ClassLong.ClassLong()
+    
+
+if sys.argv[2][0] == '+' :
+    result = a + b
+
+
+if sys.argv[2][0] == '-' :
+    result = a - b
+
+if sys.argv[2][0] == '*' :
+    result = a * b
+
+if sys.argv[2][0] == '/' :
+    result = a / b
+
+if sys.argv[2][0] == '%' :
+    result = a % b
+    
+if sys.argv[2][0] == '^' :
+    c = ClassLong.ClassLong()
+    if bin == 1 :
+	c.ReadBin(sys.argv[5])
+    else :
+	c.ReadText(sys.argv[5])
+
+    result = ClassLong.PowMod(a, b, c)
+  
+if bin == 1 :
+    result.WriteBin(sys.argv[4])
+else :
+    result.WriteText(sys.argv[4])
