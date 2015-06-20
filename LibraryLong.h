d32968c4767ba4371765ae9dad51b066172af809
sarlin <sarlinp94@gmail.com>
2015-06-20 23:49:03 +0700

    Update LibraryLong.h

--- LibraryLong.h
+++ LibraryLong.h
@@ -6,27 +6,31 @@
 
 struct LongNumber
 {
-	unsigned long long int size;
-	unsigned int* part;
+    unsigned int size;
+    unsigned int* part;
 };
 
 struct LongNumber ReadBinFile(const char* file);
 void WriteBinFile(const char* file, struct LongNumber number);
 struct LongNumber ReadTextFile(const char* file);
 void WriteTextFile(const char* file, struct LongNumber number);
+
 struct LongNumber Sum(struct LongNumber a, struct LongNumber b);
 struct LongNumber Dif(struct LongNumber a, struct LongNumber b);
 struct LongNumber Mul(struct LongNumber a, struct LongNumber b);
-struct LongNumber Div(struct LongNumber a, struct  LongNumber b, int choice);
+struct LongNumber Div(struct LongNumber a, struct LongNumber b, int sumbols);
+struct LongNumber Deg(struct LongNumber a, struct LongNumber b, struct LongNumber c);
+
+struct LongNumber DivSmall(struct LongNumber a, unsigned long long int b);
 struct LongNumber MulSmall(struct LongNumber a, unsigned long long int b);
 struct LongNumber Shift(struct LongNumber a, unsigned int s);
-struct LongNumber Deg(struct LongNumber a, struct LongNumber b, struct LongNumber c);
-struct LongNumber DivSmall(struct LongNumber a, unsigned long long int b);
 struct LongNumber Clear(struct LongNumber number);
 struct LongNumber Allocate(struct LongNumber number, unsigned int size);
+struct LongNumber Zero(struct LongNumber number, unsigned int size);
 int Compare(struct LongNumber a, struct LongNumber b);
 struct LongNumber Copy(struct LongNumber from);
-struct LongNumber Norm(struct LongNumber a);
+struct LongNumber ToNorm(struct LongNumber a);
+
 struct LongNumber ReadNum(unsigned long long int value);
 struct LongNumber ReadStr(const char* num);
 char* PrintNum(struct LongNumber number);
